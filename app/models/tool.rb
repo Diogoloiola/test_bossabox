1b0297f757a9cef719ce9dad7f33d9a3957aeadb
Diogo Loiola <diogomouraloiola@gmail.com>
2022-06-16 20:55:55 -0300

    add callback to save tags

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,9 +1,21 @@
 class Tool < ApplicationRecord
   include Filterable
 
+  attr_reader :tags
+
   has_many :tags, dependent: :destroy
 
   validates_presence_of :title, :link, :description
 
   scope :filter_by_tag, ->(value) { includes(:tags).where(tags: { description: value }) }
+
+  affter_save :save_tags
+
+  private
+
+  def save_tags
+    tags.each do |tag|
+      Tag.create!(description: tag, tool_id: id)
+    end
+  end
 end
